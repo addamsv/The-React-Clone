3b27989ba2785da54be42e0902516c5addb8ecaa
Sergey Adamovich <swares@mail.ru>
2023-03-06 14:30:20 +0300

    Delete src directory

--- src/data/data.ts
+++
@@ -1,6 +0,0 @@
-const cite: Array<{ author: string,cite: string }> = [
-  { author: 'Michael Faraday', cite: 'No matter what you look at, if you look at it closely enough, you are involved in the entire universe' },
-  { author: 'Albert Einstein', cite: 'Once have said: "There were only two things that might be infinite: the Universe and human stupidity" and he followed it by saying that he had no confidence that the Universe was infinite' }
-];
-
-export default cite;
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactDOM } from "./rct/Rct";
-
-import "./index.scss";
-import App from "./App";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
