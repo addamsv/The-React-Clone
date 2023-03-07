6268fa85fc01118246d30eecab4751a488acad49
addamsv <googolcalculator@gmail.com>
2023-03-07 13:40:19 +0300

    feat: add new tests

--- config/scripts/e2e/app.puppeteer-test.tsx
+++ config/scripts/e2e/app.puppeteer-test.tsx
@@ -21,34 +21,13 @@ describe("Main Page", () => {
   }, 10000);
 
   it('should include "inc" text on page', async () => {
-    await expect(page).toMatch("inc");
-  }, 10000);
-
-  it("should include href with correct link", async () => {
-    const hrefsArray = await page.evaluate(() =>
-      Array.from(document.querySelectorAll("a[href]"), (a) =>
-        a.getAttribute("href")
-      )
-    );
-    expect(hrefsArray[5]).toMatch("https://github.com");
+    await expect(page).toMatch("Simple React Element");
   }, 10000);
 
   it("should include the React svg correct image", async () => {
     const images = await page.$$eval("img", (anchors) =>
       [].map.call(anchors, (img) => img["src"])
     );
-    expect(images[0]).toMatch(SERVER_URL + "/assets/img/logo.svg");
+    expect(images[0]).toMatch(SERVER_URL + "/src/assets/img/logo.svg");
   }, 10000); //JEST_TIMEOUT
-});
-
-describe("Users For Test", () => {
-  beforeAll(async () => {
-    await page.goto("http://localhost:3000/users-test", {
-      waitUntil: "domcontentloaded",
-    });
-  }, 10000);
-
-  it("should include 'Users Test Page' on the page", async () => {
-    await expect(page).toMatch("Users Test Page");
-  }, 10000);
 });
