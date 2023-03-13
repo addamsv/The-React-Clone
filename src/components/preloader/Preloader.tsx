818dea6dc2dfd4384fa8ca27d1b58935c6861512
addamsv <googolcalculator@gmail.com>
2023-03-14 01:05:54 +0300

    feat: logo rotation animation

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -7,7 +7,7 @@ import "./index.scss";
 const Preloader = () => {
   return (
     <div className="preloader">
-      <img width={400} height={50} src={img} alt="logo" />
+      <img className="preloader-logo" src={img} alt="logo" />
       <h4 className="preloader-title">Loading...</h4>
       <div className="preloader-line-wrapper">
         <div className="preloader-line"></div>
